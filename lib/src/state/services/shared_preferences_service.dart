52839d7175f013601aa884ea83a18680c3da6b9f
milleradulu <milleradulu@gmail.com>
2019-10-15 06:10:47 +0300

    Setup shared preferences access
    Have the fetching occur in one place

---
+++ lib/src/state/services/shared_preferences_service.dart
@@ -0,0 +1,34 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class SharedPreferencesService {
+  Future<int> getUserId();
+  Future<String> getGovId();
+  Future<String> getName();
+  Future<String> getAccessToken();
+}
+
+class SharedPreferencesInstance implements SharedPreferencesService {
+  @override
+  Future<int> getUserId() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getInt('userId');
+  }
+
+  @override
+  Future<String> getGovId() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getString('govId');
+  }
+
+  @override
+  Future<String> getName() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getString('name');
+  }
+
+  @override
+  Future<String> getAccessToken() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getString('accessToken');
+  }
+}
--- lib/src/utils/service_locator.dart
+++ lib/src/utils/service_locator.dart
@@ -1,13 +1,17 @@
 import 'package:get_it/get_it.dart';
 
 import 'package:afyamkononi/src/state/managers/auth_manager.dart';
+
 import 'package:afyamkononi/src/state/services/api_service.dart';
+import 'package:afyamkononi/src/state/services/shared_preferences_service.dart';
 
 GetIt sl = GetIt.instance;
 
 void setUpServiceLocator() {
   // Services
   sl.registerLazySingleton<APIService>(() => APIServiceInstance());
+  sl.registerLazySingleton<SharedPreferencesService>(
+      () => SharedPreferencesInstance());
 
   // Managers
   sl.registerLazySingleton<AuthManager>(() => AuthManagerInstance());
