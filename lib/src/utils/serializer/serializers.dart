c4954ec314be6210b1308d5797c0d90b8a6a337e
milleradulu <milleradulu@gmail.com>
2019-10-16 11:04:01 +0300

    Add medical data models

--- lib/src/utils/serializer/serializers.dart
+++ lib/src/utils/serializer/serializers.dart
@@ -6,9 +6,16 @@ import 'package:built_collection/built_collection.dart';
 
 import 'package:afyamkononi/src/models/auth.dart';
 import 'package:afyamkononi/src/models/consent.dart';
+import 'package:afyamkononi/src/models/medical_data.dart';
 
 part 'serializers.g.dart';
 
-@SerializersFor(const [SignInResult, ConsentResult, ConsentResults])
+@SerializersFor(const [
+  SignInResult,
+  ConsentResult,
+  ConsentResults,
+  MedicalData,
+  MedicalRecords
+])
 final Serializers serializers =
     (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
